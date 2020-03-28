c89000e0775d7a56b1ed1aa6c4850d1e2a0fcc1a
pq <devpgq@126.com>
2020-03-28 18:14:26 +0800

    push

--- SwiftUI-project-Login/SwiftUI-project-Login/ContentView.swift
+++ SwiftUI-project-Login/SwiftUI-project-Login/ContentView.swift
@@ -29,6 +29,23 @@ struct ContentView: View {
                 
             .navigationBarTitle(Text(""), displayMode: .inline)
         }
+        .onAppear {
+            let subscriber = [1,2,3,4,5].publisher.mySink(
+                receiveCompletion: { completion in
+                    print("Completion: \(completion)")
+                },
+                receiveValue: { value in
+                    print("Receive value: \(value)")
+                    return true
+                }
+            )
+            
+            subscriber.resume()
+            subscriber.resume()
+            subscriber.resume()
+            subscriber.resume()
+            subscriber.resume()
+        }
     }
 }
 
@@ -80,3 +97,72 @@ struct TimerView: View {
     }
 }
 
+protocol Resumable {
+    func resume()
+}
+
+extension  Subscribers {
+    
+    class MySink<Input, Failure: Error>: Subscriber, Cancellable, Resumable {
+        let receiveCompletion: (Subscribers.Completion<Failure>) -> Void
+        let receiveValue: (Input) -> Bool
+        
+        var subscription: Subscription?
+        var shouldPUllNewValue: Bool = false
+        
+        init(receiveCompletion: @escaping (Subscribers.Completion<Failure>) -> Void,
+             receiveValue: @escaping (Input) -> Bool) {
+            self.receiveCompletion = receiveCompletion
+            self.receiveValue = receiveValue
+        }
+        
+        // MARK: - Subscriber
+        func receive(subscription: Subscription) {
+            self.subscription = subscription
+//            subscription.request(.unlimited)
+            subscription.request(.max(1))
+        }
+        
+        func receive(_ input: Input) -> Subscribers.Demand {
+            shouldPUllNewValue = receiveValue(input)
+            // 如果返回true，就继续返回
+            return shouldPUllNewValue ? .max(1) : .none
+        }
+        
+        func receive(completion: Subscribers.Completion<Failure>) {
+            receiveCompletion(completion)
+            subscription = nil
+        }
+        
+        
+        // MARK: - Cancellable
+        func cancel() {
+            subscription?.cancel()
+            subscription = nil
+        }
+        
+        
+        // MARK: - backpressure Resumable
+        func resume() {
+            guard !shouldPUllNewValue else {
+                return
+            }
+            shouldPUllNewValue = true
+            subscription?.request(.max(1))
+        }
+    }
+}
+
+
+
+extension Publisher {
+    func mySink(receiveCompletion: @escaping (Subscribers.Completion<Failure>) -> Void,
+                receiveValue: @escaping (Output) -> Bool) -> Cancellable & Resumable {
+        let sink = Subscribers.MySink<Output, Failure>(
+            receiveCompletion: receiveCompletion,
+            receiveValue: receiveValue
+        )
+        self.subscribe(sink)
+        return sink
+    }
+}
